21323d5a256198e34d710c5253ead55aa0b74f5b
Daniel Monyak <danmonyak@gmail.com>
2023-06-24 16:01:19 -0400

    Update useSmooth.R

--- useSmooth.R
+++ useSmooth.R
@@ -8,11 +8,17 @@ indir <- paste(repo, 'glmvc/site-analysis', paste(sample, '_inputs', sep=''), se
 ALT_readCountMatrix <- read.csv(paste(indir, 'ALT_readCountMatrix.csv', sep='/'))
 REF_readCountMatrix <- read.csv(paste(indir, 'REF_readCountMatrix.csv', sep='/'))
 
+picked_sites <- ALT_readCountMatrix[, 1]
 ALT_readCountMatrix <- ALT_readCountMatrix[, 2:ncol(ALT_readCountMatrix)]
 REF_readCountMatrix <- REF_readCountMatrix[, 2:ncol(REF_readCountMatrix)]
 
-ALT_readCountMatrix <- ALT_readCountMatrix[, 1:10]
-REF_readCountMatrix <- REF_readCountMatrix[, 1:10]
+ALT_readCountMatrix <- ALT_readCountMatrix[, 1:20]
+REF_readCountMatrix <- REF_readCountMatrix[, 1:20]
 
-make.genotype.and.probability.matrices(M.ref=REF_readCountMatrix, M.alt=ALT_readCountMatrix,
-                                       DELTA=0, num.cores=4)
+res <- make.genotype.and.probability.matrices(M.ref=REF_readCountMatrix, M.alt=ALT_readCountMatrix,
+                                       DELTA=0.5, num.cores=4)
+genotypes <- as.data.frame(res[1])
+names(genotypes) <- names(REF_readCountMatrix)
+rownames(genotypes) <- picked_sites
+
+write.csv(genotypes, paste(sample, 'smoothedGenotypes.csv', sep='-'), quote=FALSE)
